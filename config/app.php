339661184d8b9fc7fc5dd88a93d8c797401e8d56
Marinovz <marinovz@users.noreply.github.com>
2018-12-28 17:31:13 +0100

    Update app.php

--- config/app.php
+++ config/app.php
@@ -65,7 +65,7 @@ return [
     |
     */
 
-    'timezone' => env('APP_TIMEZONE', 'Europe/Italy'),
+    'timezone' => env('APP_TIMEZONE', 'Europe/San_Marino'),
 
     /*
     |--------------------------------------------------------------------------
